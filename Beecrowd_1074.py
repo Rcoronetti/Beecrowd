b8bcf5195c4e3698ac5e63fbcd1104a27796e9a4
Rafael Coronetti <104257743+Rcoronetti@users.noreply.github.com>
2022-04-30 00:11:44 -0300

    Add files via upload

---
+++ Beecrowd_1074.py
@@ -0,0 +1,33 @@
+'''Leia um valor inteiro N. Este valor será a quantidade de valores que serão lidos em seguida. Para cada valor lido, mostre uma mensagem em inglês dizendo se este valor 
+lido é par (EVEN), ímpar (ODD), positivo (POSITIVE) ou negativo (NEGATIVE). No caso do valor ser igual a zero (0), embora a descrição correta seja (EVEN NULL), 
+pois por definição zero é par, seu programa deverá imprimir apenas NULL.
+
+Entrada
+A primeira linha da entrada contém um valor inteiro N(N < 10000) que indica o número de casos de teste. Cada caso de teste a seguir é um valor inteiro X (-107 < X <107).
+
+Saída
+Para cada caso, imprima uma mensagem correspondente, de acordo com o exemplo abaixo. Todas as letras deverão ser maiúsculas e sempre deverá haver um espaço entre duas
+ palavras impressas na mesma linha.'''
+
+n=int(input())
+par=0
+impar=0
+positivo=0
+negativo=0
+for c in range(1,n+1):
+    x=int(input())
+    if x%2==0 and x>0:
+        print('EVEN POSITIVE')
+    if x%2==0 and x<0:
+        print('EVEN NEGATIVE')   
+    if x%2!=0 and x>0:
+        print('ODD POSITIVE') 
+    if x%2!=0 and x<0:
+        print('ODD NEGATIVE')
+    if x ==0:
+        print('NULL')    
+          
+
+        
+    
+    
